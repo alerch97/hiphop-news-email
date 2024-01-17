f79ed5f9dfa0145de2737ed23656e822f26a07cb
alerch97 <alexlerch76@gmail.com>
2024-01-17 19:10:51 +0100

    Add url's, specify language, changed topic

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 from send_email import send_email
 
+TOPIC = "(hiphop OR rap)"
 API_KEY = "1d91efc79c4d429fa402017e62be6ef0"
-URL = ("https://newsapi.org/v2/everything?q=tesla&from="
-       "2023-12-17&sortBy=publishedAt&apiKey=1d91efc79c4"
-       "d429fa402017e62be6ef0")
+URL = ("https://newsapi.org/v2/everything?"
+       f"q={TOPIC}&"
+       "sortBy=publishedAt&"
+       f"apiKey={API_KEY}&"
+       "language=de")
 
 # Make request
 request = requests.get(URL)
@@ -24,7 +27,9 @@ Subject: News Update, {date[0:16]}
 # Access the article titles and description + add to message
 for article in content["articles"]:
     if article["title"] is not None:
-        message = (message + f"{article['title']}" + "\n" + f"{article['description']}"
+        print(article["title"])
+        message = (message + f"{article['title']}" + "\n"
+                   + f"{article['description']}"
                    + "\n" + f"{article['url']}" + 2*"\n")
 
 message = message.encode("utf-8")
